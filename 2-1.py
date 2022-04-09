a2ed62b5747e7c61027b1ac0f7bd74c527804ea2
fivesevendev <github@fiveseven.dev>
2022-04-09 15:06:04 -0400

    wip

---
+++ 2-1.py
@@ -0,0 +1,17 @@
+#AOC-2020-
+
+
+import timeit, sys, time
+
+
+def numFind(n):
+    pass
+
+
+if __name__ == '__main__':
+    startTime = timeit.default_timer()
+    print("\n>>>>>", time.asctime(), "<<<<<")
+    n = 0
+    print("Result:", numFind(n))
+    print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
+    print(">>>>>", time.asctime(), "<<<<<")
