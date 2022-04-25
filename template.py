0be8afb43394294573bae6c4d83b04ebb5e204a6
fivesevendev <github@fiveseven.dev>
2022-04-24 21:51:07 -0400

    Update template

--- template.py
+++ template.py
@@ -15,8 +15,8 @@ def numFind(n):
 
 if __name__ == '__main__':
     startTime = timeit.default_timer()
-    print("\n>>>>>", time.asctime(), "<<<<<")
+    print(">>>>>", time.asctime(), "<<<<<\n")
     n = 0
     print("Result:", numFind(n))
     print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
-    print(">>>>>", time.asctime(), "<<<<<")
+    print("\n>>>>>", time.asctime(), "<<<<<")
